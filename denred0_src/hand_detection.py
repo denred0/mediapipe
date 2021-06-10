86cb854a56f4180b200fc22c37943d4e7cd2d76c
vid <hah@videomatrix.ru>
2021-06-10 11:39:32 +0500

    -

--- denred0_src/hand_detection.py
+++ denred0_src/hand_detection.py
@@ -2,6 +2,9 @@ import cv2
 import mediapipe as mp
 import os
 from tqdm import tqdm
+from pathlib import Path
+
+from os import walk
 
 from mediapipe2.python.solutions import hands as mediapipe_hands
 
@@ -58,7 +61,7 @@ def example():
 
             # Print handedness and draw hand landmarks on the image.
             # print('Handedness:', results.multi_handedness)
-            if not results.multi_hand_landmarks:
+            if not results.hand_rects:
                 continue
             image_height, image_width, _ = image.shape
             annotated_image = image.copy()
@@ -82,7 +85,8 @@ def example():
 
                 hand_img = image[y:y2, x:x2]
 
-                cv2.imwrite(data_dir + '/hands_crop/' + tag + '_' + str(idx) + '_' + str(i) + '.jpg', cv2.flip(hand_img, 1))
+                cv2.imwrite(data_dir + '/hands_crop/' + tag + '_' + str(idx) + '_' + str(i) + '.jpg',
+                            cv2.flip(hand_img, 1))
 
                 annotated_image = cv2.rectangle(annotated_image, (x, y), (x2, y2), color, thickness)
 
@@ -101,5 +105,72 @@ def example():
             #         annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
 
+def example_comp():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mediapipe_hands
+
+    data_dir = 'data_comp/hands_comp'
+    tag = 'tst_bio'
+
+    # For static images:
+    with mp_hands.Hands(
+            static_image_mode=True,
+            max_num_hands=6,
+            min_detection_confidence=0.6) as hands:
+
+        for subdir, dirs, files in os.walk(data_dir):
+            for k, folder in tqdm(enumerate(dirs), total=len(dirs)):
+                Path('data_comp/hands_rect/' + str(folder)).mkdir(parents=True, exist_ok=True)
+                Path('data_comp/hands_crop/' + str(folder)).mkdir(parents=True, exist_ok=True)
+
+                p = data_dir + '/' + folder + '/'
+                _, _, images_list = next(walk(p))
+
+                for file in images_list:
+
+                    filename, file_extension = os.path.splitext(file)
+
+                    # Read an image, flip it around y-axis for correct handedness output (see
+                    # above).
+                    image = cv2.flip(cv2.imread(os.path.join(data_dir, folder, file)), 1)
+                    # Convert the BGR image to RGB before processing.
+                    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+                    # Print handedness and draw hand landmarks on the image.
+                    # print('Handedness:', results.multi_handedness)
+                    if not results.hand_rects:
+                        continue
+                    image_height, image_width, _ = image.shape
+                    annotated_image = image.copy()
+
+                    for i, rect in enumerate(results.hand_rects):
+                        color = (255, 0, 255)
+                        # if results.multi_handedness[i].classification[0].label == 'Right':
+                        #     color = (255, 0, 255)
+                        # else:
+                        #     color = (0, 255, 255)
+
+                        thickness = 6
+
+                        decrease_box = 0
+
+                        x = abs(int((rect.x_center * image_width - rect.width * image_width / 2) * (1 + decrease_box)))
+                        y = abs(
+                            int((rect.y_center * image_height - rect.height * image_height / 2) * (1 + decrease_box)))
+
+                        x2 = abs(int((rect.x_center * image_width + rect.width * image_width / 2) * (1 - decrease_box)))
+                        y2 = abs(
+                            int((rect.y_center * image_height + rect.height * image_height / 2) * (1 - decrease_box)))
+
+                        hand_img = image[y:y2, x:x2]
+
+                        cv2.imwrite('data_comp/hands_crop/' + folder + '/' + filename + '_' + str(i) + file_extension,
+                                    cv2.flip(hand_img, 1))
+
+                        annotated_image = cv2.rectangle(annotated_image, (x, y), (x2, y2), color, thickness)
+
+                    cv2.imwrite('data_comp/hands_rect/' + folder + '/' + file, cv2.flip(annotated_image, 1))
+
+
 if __name__ == '__main__':
-    example()
+    example_comp()
